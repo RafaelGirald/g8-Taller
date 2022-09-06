50425daff58becd1ce50b169f443e3a5b9814934
unknown <angelgiraldo96@gmail.com>
2022-09-05 22:25:21 -0500

    carga inicial

--- Taller.App/Taller.App.Front/Pages/Registro/GestionRegistro.cshtml.cs
+++ Taller.App/Taller.App.Front/Pages/Registro/GestionRegistro.cshtml.cs
@@ -5,6 +5,9 @@ namespace Taller.App.Front.Pages
 {
     public class GestionRegistroModel : PageModel
     {
+        public List <string> departamentos = new List<string>(){
+            "Antioquia","Quindio","Cundinamarca","Tolima" 
+        };
         public void OnGet()
         {
         }
